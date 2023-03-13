f746bd4daa67df3155d357e515e68ceb4fd9b117
raffiner12 <1soigne2@gmail.com>
2023-03-14 00:15:34 +0900

    로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,11 +9,26 @@ function LandingPage() {
      .then(response => { console.log(response.data) }) // 서버에서 돌아오는 response를 콘솔창에 보여줌.
   }, [])
 
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if(response.data.success) {
+          props.history.push("/login")
+        } else {
+          alert('로그아웃하는 데 실패했습니다.')
+        }
+      })
+  }
+
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   )
 }
